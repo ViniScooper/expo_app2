04fc5454fb5e9af7aa026e8aab4caa7200e4a67b
ViniScooper <vviniciuslourenco@gmail.com>
2025-05-12 21:34:33 -0300

    app de academia

---
+++ ignitegym/App.tsx
@@ -0,0 +1,54 @@
+
+import {  StatusBar } from 'react-native';
+
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular
+
+} from "@expo-google-fonts/roboto";
+
+import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed'
+
+import { config } from "./config/gluestack-ui.config";
+
+import { Loading } from '@components/Loading';
+
+
+
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+
+  return (
+    <GluestackUIProvider config={config}>
+
+
+
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent={true}
+
+      />
+
+
+      {!fontsLoaded ?
+
+        (<Center flex ={1} bg="$gray700">
+
+          <Text>Home</Text>
+
+        </Center>
+
+
+        ) : (
+        
+        <Loading />
+        
+        )}
+
+    </GluestackUIProvider>
+  )
+}
